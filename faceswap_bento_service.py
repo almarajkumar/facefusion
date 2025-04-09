d93fa4f2d052b80e73c50072c8d3af09dbd6de46
rajkumaralma <raj@flyingcaps.com>
2025-04-09 19:33:33 +0530

    Enhance

--- faceswap_bento_service.py
+++ faceswap_bento_service.py
@@ -25,8 +25,7 @@ class FaceSwapModel:
 
             command = f"""
                 python3 facefusion.py headless-run -s {src_path}  -t {tgt_path} -o {output_path} \
-                --execution-providers cuda \
-                --processors face_swapper face_enhancer
+                --processors face_swapper face_enhancer --execution-providers cuda
             """
             print(f"Running command: {command}")
 
